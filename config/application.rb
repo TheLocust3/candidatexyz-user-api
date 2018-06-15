c9063da71061e95e74b2f7f41f7ce3a0e0e2c76f
TheLocust3 <jake.kinsella@gmail.com>
2018-06-15 17:02:36 -0400

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,30 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
+
+Bundler.require(*Rails.groups)
+
+module UserApi
+  class Application < Rails::Application
+    config.load_defaults 5.1
+
+    config.api_only = true
+
+    Jbuilder.key_format camelize: :lower
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: :any
+      end
+    end
+  end
+end
